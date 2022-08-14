e2ecf445cd466947dd1e0c95bec33b7b922ca995
naneps <ekaprasetya2244@gmail.com>
2022-08-15 00:49:44 +0700

    AGJ 41

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -8,6 +8,7 @@
   <!-- General CSS Files -->
 
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{ asset('assets/fontawesome-free-6.1.2-web/css/all.min.css') }}">
 
   <!-- CSS Libraries -->
   {{-- <link rel="stylesheet" href="../../../node_modules/jqvmap/dist/jqvmap.min.css">
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,57 +2,54 @@
      <div class="main-sidebar sidebar-style-2">
         <aside id="sidebar-wrapper">
           <div class="sidebar-brand">
-            <a href="index.html">AGJ</a>
+            <img src="{{ asset('assets/images/logo.png') }}"  class="avatar img-responsive"  alt="">
           </div>
           <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">AGJ</a>
           </div>
           <ul class="sidebar-menu">
               <li class="menu-header">Menu Utama</li>
-              <li class="nav-item active">
-                <a href="dashboard.html" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+
+              <li class="nav-item">
+                <a href="data-pegawai.html" class="nav-link"><i class="fa-solid fa-chart-line"></i><span>Dashboard</span></a>
               </li>
               <li class="nav-item">
-                <a href="data-pegawai.html" class="nav-link"><i class="fas fa-fire"></i><span>Data Pegawai</span></a>
+                <a href="data-pegawai.html" class="nav-link"><i class="fa-solid fa-people-group"></i><span>Data Pegawai</span></a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Data Customer</span></a>
+                <a href="#" class="nav-link"><i class="fa-solid fa-people-roof"></i><span>Data Customer</span></a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Transaksi</span></a>
+                <a href="#" class="nav-link"><i class="fa-solid fa-cash-register"></i><span>Transaksi</span></a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Pembayaran</span></a>
+                <a href="#" class="nav-link"><i class="fa-solid fa-wallet"></i><span>Pembayaran</span></a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dokumen Customer</span></a>
+                <a href="#" class="nav-link"><i class="fa-solid fa-id-card-clip"></i><span>Dokumen Customer</span></a>
               </li>
               <li class="menu-header">Data Master</li>
               <li class="nav-item">
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Profile Perusahaan</span></a>
+                <a href="#" class="nav-link"><i class="fa-solid fa-building"></i><span>Profile Perusahaan</span></a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Harga Dasar Bangunan</span></a>
+                <a href="#" class="nav-link"><i class="fa-solid fa-building-columns"></i><span>Harga Dasar Bangunan</span></a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Harga Dasar Tanah</span></a>
+                <a href="#" class="nav-link"><i class="fa-solid fa-signs-post"></i><span>Harga Dasar Tanah</span></a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Luas Tanah</span></a>
+                <a href="#" class="nav-link"><i class="fa-solid fa-maximize"></i><span>Luas Tanah</span></a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Tipe Rumah</span></a>
+                <a href="#" class="nav-link"><i class="fa-solid fa-house-circle-check"></i><span>Tipe Rumah</span></a>
               </li>
               <li class="menu-header">Cetak</li>
               <li class="nav-item">
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Laporan Transaksi</span></a></li>
+                <a href="#" class="nav-link"><i class="fa-solid fa-money-bill-trend-up"></i><span>Laporan Transaksi</span></a></li>
+              <li class="nav-item">
+                <a href="#" class="nav-link"><i class="fas fa-gear"></i><span>Settings</span></a></li>
             </ul>
 
-
-            <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-              <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-                <i class="fas fa-rocket"></i> Documentation
-              </a>
-            </div>
         </aside>
       </div>
