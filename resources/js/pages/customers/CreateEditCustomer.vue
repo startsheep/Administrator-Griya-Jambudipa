074e4e1e45f832be021106bd252c6195a1100b2c
naneps <ekaprasetya2244@gmail.com>
2022-08-21 13:56:16 +0700

    👌can crud customer

---
+++ resources/js/pages/customers/CreateEditCustomer.vue
@@ -0,0 +1,353 @@
+<template>
+  <Transition name="fade">
+    <section class="section">
+      <div class="section-header">
+        <h1>{{ id ? "Edit" : "Tambah" }} Data Pegawai</h1>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-header"></div>
+            <div class="card-body">
+              <form class="form-row">
+                <div class="form-group col-lg-6">
+                  <label for="name">Nama Lengkap</label>
+                  <input
+                    v-model="customer.name"
+                    type="text"
+                    class="form-control"
+                    placeholder="Nama Lengkap"
+                  />
+                </div>
+
+
+                <div class="form-group col-lg-6">
+                  <label>Pekerjaan</label>
+                   <input
+                    v-model="customer.profession"
+                    type="text"
+                    class="form-control"
+                    placeholder="Pekerjaan"
+                  />
+                </div>
+                <div class="form-group col-lg-6">
+                  <label>Email</label>
+                  <input
+                    v-model="customer.email"
+                    type="text"
+                    class="form-control"
+                    placeholder="Email"
+                  />
+                </div>
+                <div class="form-group col-lg-6">
+                  <label>Nomor Telepon</label>
+                  <input
+                    v-model="customer.phone"
+                    type="number"
+                    class="form-control"
+                    placeholder="Nomor Telepon"
+                  />
+                </div>
+
+                <div class="form-group col-lg-6">
+                  <label>Jenis Kelamin</label>
+                  <select
+                    v-model="customer.gender"
+                    class="form-control form-control"
+                  >
+                    <option value="L">Laki-Laki</option>
+
+                    <option value="P">Perempuan</option>
+                  </select>
+                </div>
+
+                <div class="form-group col-lg-6">
+                 <label>Blok Kavling</label>
+                  <select
+                    v-model="customer.kavling_id"
+                    class="form-control form-control"
+                  >
+                    <option v-for="kavling in kavlings" :key="kavling.id" :value="kavling.id">{{ kavling.block }} - {{ kavling.numberKavling }}</option>
+
+
+                  </select>
+                </div>
+                <div class="custom-file form-group col-lg-6">
+                  <input
+                    @change="selectImage"
+                    type="file"
+                    class="custom-file-input"
+                  />
+                  <label class="custom-file-label" for="customFile"
+                    >Pilih Foto</label
+                  >
+                </div>
+                <div class="custom-file form-group col-lg-6">
+                  <input
+                    @change="selectDocuments"
+                    type="file"
+                    class="custom-file-input"
+                    multiple
+                  />
+                  <label class="custom-file-label" for="customFile"
+                    >Pilih Dokumen</label
+                  >
+                  <div v-if="customer.document" class="form-group col-lg-6">
+                    <!-- -->
+                  </div>
+                </div>
+
+<div class="form-group col-lg-12">
+                  <label>Alamat</label>
+                  <textarea
+                    v-model="customer.address"
+                    class="form-control"
+                    rows="1"
+                  ></textarea>
+                </div>
+
+              </form>
+              <div class="row">
+                <div class="col-lg-6">
+                  <div v-if="customer.image" class="badge badge-primary mt-2">
+                    {{ customer.image.name }}
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <div v-for="document in customer.document" :key="document" class="badge badge-primary m-1 p-2">
+                    {{ document.name }} <i class="fas fa-times sortable" @click="removeDocument(document)"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card-footer">
+              <div class="d-flex justify-content-center">
+                <button @click="back" class="btn btn-danger mr-2" type="button">
+                  Batal
+                </button>
+                <button
+                  :class="{ 'disabled btn-progress': isSubmit }"
+                  @click="id ? updateCustomer() : createCustomer()"
+                  class="btn btn-primary ml-2"
+                  type="button"
+                >
+                  {{ id ? "Update" : "Tambah" }}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Transition>
+</template>
+<script>
+import iziToast from "izitoast";
+import moment from "moment";
+
+export default {
+  props: ["id"],
+  data() {
+    return {
+      customer: {
+        name: "",
+        email: "",
+        profession: "",
+        kavling_id: "",
+        phone : "",
+        address: "",
+        gender: "",
+        image: "",
+        document: [],
+      },
+      previewImage: "",
+      kavlings: [],
+      isSubmit: false,
+    };
+  },
+  mounted: function () {
+    this.getKavlings();
+    if (this.id) {
+      this.showCustomer();
+    }
+  },
+  computed: {
+    formData() {
+      const fieldData = new FormData();
+      if (this.id) {
+        fieldData.append("id", this.id);
+        fieldData.append("_method", "PUT");
+      }
+      fieldData.append("name", this.customer.name);
+      fieldData.append("email", this.customer.email);
+      fieldData.append("kavling_id", this.customer.kavling_id);
+      fieldData.append("profession", this.customer.profession);
+      fieldData.append("phone", this.customer.phone);
+      fieldData.append("address", this.customer.address);
+      fieldData.append("gender", this.customer.gender);
+      this.customer.document.forEach((document , index) => {
+        fieldData.append("documents["+index+"]", document);
+      });
+    //   fieldData.append("documents[]", this.customer.document);
+      if (this.customer.image) {
+        fieldData.append("image", this.customer.image);
+      }
+      return fieldData;
+    },
+  },
+  methods: {
+    back() {
+      this.$router.back();
+    },
+    removeDocument(document) {
+      this.customer.document.splice(this.customer.document.indexOf(document), 1);
+    },
+    checkExtension(file) {
+      const allowedExtensions = ["image/jpg", "image/png", "image/jpeg"];
+      const extension = file.type;
+      if (allowedExtensions.includes(extension)) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    checkIsDocument(file) {
+      const allowedExtensions = [
+        "image/jpg",
+        "image/png",
+        "image/jpeg",
+        "application/pdf",
+        "application/msword",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      ];
+      const extension = file.type;
+      if (allowedExtensions.includes(extension)) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+
+    selectDocuments(e) {
+      const files = e.target.files;
+
+      for (let i = 0; i < files.length; i++) {
+        if (this.checkIsDocument(files[i])) {
+          this.customer.document.push(files[i]);
+        } else {
+          iziToast.warning({
+            title: "Peringatan",
+            message: "File harus berupa dokumen",
+            position: "topRight",
+          });
+        }
+      }
+      console.log(this.customer.document);
+    },
+    selectImage(e) {
+      console.log(e.target.files);
+      const file = e.target.files[0];
+      if (this.checkExtension(file)) {
+        this.customer.image = file;
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          this.previewImage = e.target.result;
+        };
+        reader.readAsDataURL(file);
+      } else {
+        iziToast.warning({
+          title: "Warning",
+          message: "File harus berformat jpg, png, jpeg",
+          position: "topRight",
+        });
+      }
+    },
+
+    getKavlings() {
+      const self = this;
+      self.$store.dispatch("getData", ["kavling"]).then((response) => {
+          self.kavlings = response.data;
+          console.log(this.kavlings);
+
+      });
+    },
+    createCustomer() {
+      const self = this;
+      this.isSubmit = true;
+      let type = "postDataUploadCustomer";
+      let fieldData = this.formData;
+      console.log(this.customer.document);
+      self.$store
+        .dispatch(type, fieldData, "customer")
+        .then((res) => {
+          this.isSubmit = false;
+          this.$router.back();
+          iziToast.success({
+            title: "Success",
+            message: "Data berhasil ditambahkan",
+            position: "topRight",
+          });
+        })
+        .catch((err) => {
+          this.isSubmit = false;
+          let meta = err.response.data.meta;
+          let messages = err.response.data.meta.message;
+          Object.entries(messages).forEach(([key, value]) => {
+            iziToast.warning({
+              title: "Warning",
+              message: value,
+              position: "topRight",
+            });
+          });
+        });
+    },
+    showCustomer() {
+      const self = this;
+      self.$store
+        .dispatch("getData", ["/customer/" + self.id])
+        .then((response) => {
+            this.previewImage = response.data.image;
+          self.customer.name = response.data.name;
+          self.customer.email = response.data.email;
+          self.customer.profession = response.data.profession;
+          self.customer.kavling_id = response.data.kavlingId;
+          self.customer.entry_date = response.data.entryDate;
+          self.customer.phone = response.data.phone;
+          self.customer.address = response.data.address;
+          self.customer.gender = response.data.gender;
+        });
+    },
+    updateCustomer() {
+      const self = this;
+      this.isSubmit = true;
+      let type = "updateDataUploadCustomer";
+
+      const fieldData = this.formData;
+      self.$store
+        .dispatch(type, fieldData, ["customer/" + self.id])
+        .then((res) => {
+          this.isSubmit = false;
+          this.$router.back();
+          iziToast.success({
+            title: "Success",
+            message: "Data berhasil diubah",
+            position: "topRight",
+          });
+        })
+        .catch((err) => {
+          this.isSubmit = false;
+            let meta = err.response.data.meta;
+          let messages = err.response.data.meta.message;
+          Object.entries(messages).forEach(([key, value]) => {
+            iziToast.warning({
+              title: "Warning",
+              message: value,
+              position: "topRight",
+            });
+          });
+        });
+    },
+  },
+};
+</script>
+
---
+++ resources/js/pages/customers/Customers.vue
@@ -0,0 +1,272 @@
+<template>
+  <section class="section">
+    <div class="section-header">
+      <h1>Data Customer</h1>
+    </div>
+    <div class="row">
+      <div class="col-12 col-lg-12">
+        <div class="card">
+          <div class="card-header">
+            <!-- <button class="btn btn-primary trigger--fire-modal-2" id="modal-2">Tambah Data</button> -->
+            <!-- <button tag="router-link" to="/e    mployee/create" class="btn btn-primary">Tambah Data</button> -->
+            <router-link to="customer/create" class="btn btn-primary"
+              >Tambah Data</router-link
+            >
+
+            <div class="card-header-action"></div>
+          </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="btn-group mb-3">
+                  <button
+                    type="button"
+                    class="btn btn-icon icon-rigth btn-primary mr-2"
+                  >
+                    EXPORT PDF <i class="fa-solid fa-file-pdf"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-icon icon-rigth btn-primary mr-2"
+                  >
+                    EXPORT EXCEL <i class="fa-solid fa-file-excel"></i>
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-icon icon-rigth btn-primary mr-2"
+                  >
+                    PRINT DATA <i class="fa-solid fa-print"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <input
+                  v-on:keyup="search"
+                  v-model="name"
+                  class="form-control"
+                  placeholder="Search"
+                />
+              </div>
+            </div>
+            <div class="table-responsive">
+              <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <td>
+                      <div class="custom-checkbox custom-control">
+                        <input
+                          type="checkbox"
+                          data-checkboxes="mygroup"
+                          data-checkbox-role="dad"
+                          class="custom-control-input"
+                          id="checkbox-all"
+                        />
+                        <label for="checkbox-all" class="custom-control-label"
+                          >&nbsp;</label
+                        >
+                      </div>
+                    </td>
+                    <th>Nama</th>
+                    <th>Email</th>
+                    <th>Nomor Telepon</th>
+                    <th>Blok dan nomor kavling</th>
+                    <th class="text-center">Aksi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="customer in customers" :key="customer.id">
+                    <td>
+                      <div class="custom-checkbox custom-control">
+                        <input
+                          type="checkbox"
+                          data-checkboxes="mygroup"
+                          class="custom-control-input"
+                          id="checkbox-1"
+                        />
+                        <label for="checkbox-1" class="custom-control-label"
+                          >&nbsp;</label
+                        >
+                      </div>
+                    </td>
+                    <td width="30%" class="align-middle">
+                      <div class="media">
+                        <img
+                          style="object-fit: contain; width: 50px; height: 50px"
+                          class="img-thumbnail rounded-circle mr-2"
+                          :src="'storage/' + customer.image"
+                          alt=""
+                        />
+                        <div class="media-body">
+                          <div class="media-title">{{ customer.name }}</div>
+                          <div class="text-job text-muted">
+                            <span>{{ showLogUpdate(customer.updatedAt) }}</span>
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td width="20%" class="align-middle">
+                      <span>{{ customer.email }}</span>
+                    </td>
+
+                    <td width="20%" class="align-middle">
+                      <span>{{ customer.phone }}</span>
+                    </td>
+
+                    <td width="20%" class="align-middle">
+                      <div
+                        v-if="customer.kavling.block"
+                        class="badge badge-success"
+                      >
+                        {{ customer.kavling.block }}-{{
+                          customer.kavling.numberKavling
+                        }}
+                      </div>
+                      <div v-else class="badge badge-danger">
+                        Belum Transaksi
+                      </div>
+                    </td>
+                    <td width="10%" class="align-middle text-center">
+                      <div class="show">
+                        <button
+                          data-toggle="dropdown"
+                          class="btn btn-transparent"
+                        >
+                          <i
+                            class="
+                              fa-solid fa-ellipsis-vertical
+                              dropdown-toggle
+                            "
+                            aria-expanded="true"
+                          ></i>
+                        </button>
+                        <div class="dropdown-menu action">
+                          <router-link
+                            class="dropdown-item action sortable btn"
+                            :to="'/customer/' + customer.id + '/edit'"
+                            >Edit</router-link
+                          >
+                          <router
+                            class="dropdown-item action sortable btn"
+                            @click="deleteCustomer(customer.id)"
+                            >Hapus</router
+                          >
+                          <span class="dropdown-item action sortable"
+                            >Detail</span
+                          >
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <CircleLoader v-if="isLoading" />
+            </div>
+          </div>
+          <div class="card-footer">
+            <Pagination
+              :currentPage="pagination.currentPage"
+              :rowsTotal="pagination.total"
+              :lastPage="pagination.lastPage"
+              @onPageChange="onPageChange($event)"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import iziToast from "izitoast";
+import moment from "moment";
+import Pagination from "../../components/Pagination.vue";
+import Utils from "../../store/services/utils";
+import CircleLoader from "../../components/CircleLoader.vue";
+
+export default {
+  data() {
+    return {
+      customers: [],
+      // edit
+      isLoading: false,
+      // filter
+      name: "",
+      order_direction: "asc",
+      //Pagination
+      pagination: {
+        total: 0,
+        perPage: 5,
+        currentPage: 1,
+        lastPage: 0,
+        page: 0,
+      },
+    };
+  },
+  mounted() {
+    this.getCustomers();
+  },
+  computed: {},
+  methods: {
+    showLogUpdate(date) {
+      return moment(date).fromNow();
+    },
+    formatRupiah(number) {
+      return Utils.formatRupiah(number, "Rp. ");
+    },
+    search() {
+      this.getCustomers();
+    },
+    getCustomers() {
+      const self = this;
+      this.isLoading = true;
+      const params = [
+        `name=${this.name}`,
+        // `position=${this.name}`,
+        `order_by=positions.id`,
+        `order_direction=${this.order_direction}`,
+        `page=${this.pagination.page}`,
+        `per_page=${this.pagination.perPage}`,
+      ].join("&");
+      self.$store.dispatch("getData", ["customer", params]).then((response) => {
+        self.customers = response.data;
+        self.pagination.total = response.meta.total;
+        self.pagination.currentPage = response.meta.currentPage;
+        self.pagination.lastPage = response.meta.lastPage;
+        self.isLoading = false;
+        console.log(response);
+      });
+    },
+    deleteCustomer(id) {
+      const self = this;
+      this.$swal
+        .fire({
+          title: "Yakin ?",
+          text: "Data akan dihapus",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonText: "Hapus!",
+          cancelButtonText: "Batal",
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            self.$store
+              .dispatch("deleteData", ["customer", id])
+              .then((response) => {
+                iziToast.success({
+                  title: "Success",
+                  message: "Data berhasil dihapus",
+                  position: "topRight",
+                });
+                self.getCustomers();
+              });
+          }
+        });
+    },
+
+    onPageChange(page) {
+      this.pagination.page = page;
+      this.getCustomers();
+    },
+  },
+  components: { Pagination, CircleLoader },
+};
+</script>
