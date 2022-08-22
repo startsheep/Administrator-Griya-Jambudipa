45eda683d3d32f4c946dfb24ac77c2433b1b1fec
naneps <ekaprasetya2244@gmail.com>
2022-08-22 20:28:29 +0700

    ➡️Re Usable Component

---
+++ resources/js/components/SummerNote.vue
@@ -0,0 +1,37 @@
+<template >
+  <ckeditor
+    :editor="editor"
+    v-model="editorData"
+    :config="editorConfig"
+  ></ckeditor>
+</template>
+<script>
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+export default {
+  components: {
+    // Use the <ckeditor> component in this view.
+    // ckeditor: CKEditor.component
+  },
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorData: "",
+      editorConfig: {
+        // The configuration of the editor.
+        // height: '500px',
+      },
+    };
+  },
+  methods: {
+    onChange(event) {
+      const value = this.editor.getData();
+      this.$emit("update:modelValue", value);
+    },
+  },
+};
+</script>
+<style >
+.ck-editor__editable {
+  min-height: 200px;
+}
+</style>
