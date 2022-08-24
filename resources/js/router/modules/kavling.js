014d176f6f2dc8192023a477ebf6906fdff3d378
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-08-24 23:14:17 +0700

    ⬆️Adding Route

---
+++ resources/js/router/modules/kavling.js
@@ -0,0 +1,10 @@
+export default[
+    {
+        path: "/kavlings",
+        name: "kavling",
+        component: () => import("../../pages/kavling/Kavling.vue"),
+        meta: {
+            title: "Customers",
+        }
+    },
+]
