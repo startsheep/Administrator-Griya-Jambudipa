589a1838233be0e607a393a110bc3adba59b625c
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-08-30 14:50:50 +0700

    👌Create Detail

---
+++ resources/js/pages/customers/DetailCustomer.vue
@@ -0,0 +1,181 @@
+<template>
+  <div
+    class="modal fade"
+    id="detailCustomer"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="exampleModalCenterTitle"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content bg-primary">
+        <div class="modal-header">
+          <h5 class="modal-title text-white" id="exampleModalLongTitle">
+            Detail Customer
+          </h5>
+          <button
+            type="button"
+            class="close text-white"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <CircleLoader v-if="isLoading" />
+          <div v-else class="card bg-primary">
+            <div class="card-body">
+              <div class="user-item">
+                <img
+                  alt="image"
+                  :src="'storage/' + customer.image"
+                  class="img-fluid"
+                  style="width: 150px"
+                />
+                <div class="user-details">
+                  <div class="user-name text-white">{{ customer.name }}</div>
+                  <div class="text-job text-muted">{{ }}</div>
+                  <div class="row mt-3">
+                    <div class="col text-left">
+                      <div class="text-bold">Email</div>
+                      <div class="">
+                        {{ customer.email }}
+                      </div>
+                    </div>
+                    <div class="col text-left">
+                      <div class="">Telepon</div>
+                      <div class="">
+                        {{ customer.phone }}
+                      </div>
+                    </div>
+                    <div class="col text-left">
+                      <div class="">Pekerjaan</div>
+                      <div class="">
+                        {{ customer.profession }}
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row mt-2">
+                    <div class="col text-left">
+                      <div class="">Blok / Nomor Kavling</div>
+                      <div class="">
+                        {{ showKavlings(customer.customerKavling) }}
+                      </div>
+                      <div class="mt-2">
+                        <div class="">Alamat</div>
+                        <div class="">
+                          {{
+                            customer.address
+                          }}
+                        </div>
+                      </div>
+                      <div class="mt-2">
+                        <div class="">Jenis Kelamin</div>
+                        <div class="">
+                          {{
+                            customer.gender == "L" ? "Laki-Laki" : "Perempuan"
+                          }}
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col text-left">
+                      <div class="">Dokumen</div>
+                      <div>
+                        <ul class="list-group">
+                          <li
+                            class="
+                              list-group-item
+                              d-flex
+                              justify-content-between
+                              list-group-item-secondary
+                            "
+                            v-for="doc in customer.document"
+                            :key="doc.id"
+                          >
+                            {{ doc.documentName }}
+                            <button
+                              type="button"
+                              class="btn btn-sm btn-transparent"
+                              click
+                            >
+                              <i class="fas fa-download"></i>
+                            </button>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card-footer">
+                 <button
+                    style="width: 100%;"
+                 type="button" class="btn btn-danger" data-dismiss="modal">
+            Tutup
+          </button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import CircleLoader from '../../components/CircleLoader.vue';
+export default {
+    props: ["id"],
+
+    data() {
+        return {
+            customer: {},
+            isLoading: false,
+
+        };
+    },
+
+    watch : {
+        id(newVal){
+            this.getCustomer();
+        }
+    },
+    methods: {
+        showKavlings(kavlings) {
+            if (kavlings) {
+                return kavlings
+                    .map((kavling) => {
+                    return (kavling.kavling.block + " / " + kavling.kavling.numberKavling);
+                })
+                    .join(", ");
+            }
+            else {
+                return "-";
+            }
+        },
+        downloadDoc(doc) {
+            const self = this;
+        },
+        getCustomer() {
+            this.isLoading = true;
+            const self = this;
+            self.$store.dispatch("showData", ["customer/" +self.id]).then(function (result) {
+
+                self.customer = result.data;
+                self.isLoading = false;
+            });
+        },
+    },
+    components: { CircleLoader }
+};
+</script>
+<style >
+    .list-group .list-group-item{
+
+          padding: 5px;
+          margin-top: 2px;
+          color: #000;
+
+    }
+</style>
