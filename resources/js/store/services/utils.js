2887ba2b6cc76e95b413b7cf0d84d0e190888671
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-03 15:28:45 +0700

    👌👌👌

--- resources/js/store/services/utils.js
+++ resources/js/store/services/utils.js
@@ -24,6 +24,20 @@ const Utils = {
     parseHtmlFromEditor(html){
         return html.replace(/<[^>]*>/g, '');
     },
+    formatNumberToRoman(num){
+        if (typeof num !== 'number')
+            return false;
+        var digits = String(+num).split(""),
+            key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+                "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+                "","I","II","III","IV","V","VI","VII","VIII","IX"],
+            roman_num = "",
+            i = 3;
+        while (i--)
+            roman_num = (key[+digits.pop() + (i * 10)] || "") + roman_num;
+        return Array(+digits.join("") + 1).join("M") + roman_num;
+
+    }
 
 
 }
