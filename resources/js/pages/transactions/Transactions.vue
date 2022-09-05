22fb8404193304563f5e4ef00dad218c1e199b35
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-05 14:57:25 +0700

    🔃🔃🔃🔃

---
+++ resources/js/pages/transactions/Transactions.vue
@@ -0,0 +1,40 @@
+<template>
+   <section class="section">
+    <div class="section-header">
+        <h4>Transaksi</h4>
+    </div>
+    <div class="section-body">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <td>p</td>
+                                        <td>p</td>
+                                        <td>p</td>
+                                        <td>p</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   </section>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
