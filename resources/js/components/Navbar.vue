3980457bb7a04d69409e159f4eee92fb1c458d91
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-06 20:49:29 +0700

    ⬆️⬆️

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template >
 
 
- <div class="navbar-bg-white"></div>
-      <nav class="navbar navbar-expand-lg main-navbar">
+ <div class="navbar-bg-primary" style="position:sticky; top: 0; z-index:999;">
+      <nav class="navbar  navbar-expand-lg main-navbar" >
         <form class="form-inline mr-auto">
           <ul class="navbar-nav mr-3">
             <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg "><i class="fas fa-bars "></i></a></li>
@@ -12,7 +12,7 @@
         <ul class="navbar-nav navbar-right">
           <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
             <img alt="image" src="../../../public/assets/images/avatar/avatar-2.png" class="rounded-circle mr-1">
-            <div class="d-sm-none d-lg-inline-block profile-name">Hi, Ujang Maman</div></a>
+            <div class="d-sm-none d-lg-inline-block profile-name">Halo , {{ username }}</div></a>
             <div class="dropdown-menu dropdown-menu-right">
               <div class="dropdown-title">Logged in 5 min ago</div>
               <a href="features-profile.html" class="dropdown-item has-icon">
@@ -32,11 +32,26 @@
           </li>
         </ul>
       </nav>
+</div>
 </template>
 <script>
 import Cookie from "js-cookie";
 export default {
+    data(){
+        return {
+            user : this.userCookie
+        }
+    },
+    computed:{
+        username(){
+            return this.user
+        },
+        userCookie(){
+            return Cookie.get('user');
+        }
+    },
     methods:{
+
         logout() {
             axios
                 .post(
