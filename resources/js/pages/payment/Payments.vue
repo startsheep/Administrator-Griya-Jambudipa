bba36d9ee4f8022d8d3b8aa1ca2d6a15f48845de
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-07 18:37:38 +0700

    🔃🔃🔃

--- resources/js/pages/payment/Payments.vue
+++ resources/js/pages/payment/Payments.vue
@@ -21,10 +21,10 @@
       <div class="row">
         <div class="col">
           <div class="collapse " id="formCreate">
-            <div class="col d-flex justify-content-center">
+            <div class="row d-flex justify-content-center">
                   <div class="card" style="width:80%">
               <div class="card-header">
-                <h4>Formulir Pembayaran / Cicilan </h4>
+                <h4>Formulir Pembayaran / Setoran </h4>
               </div>
               <div class="card-body">
                 <div class="alert alert-primary alert-dismissible show fade mb-4">
@@ -32,66 +32,91 @@
                         <button class="close" data-dismiss="alert">
                           <span>×</span>
                         </button>
-                        <b>Info!</b> Jika anda ingin melakukan pembayaran, silahkan isi form dibawah ini.
+                        <b>Info!</b> Form ini merupakan untuk penyetoran / Pembayaram customer.
                       </div>
                     </div>
                 <div class="form-col">
-                  <div class="form-group">
-                    <label>Nama</label>
-                    <select @change="selectCustomer(this)" ref="fieldCustomer" class="form-control" v-model="payment.customerId">
-                      <option
-                        :key="customer.id"
-                        v-for="customer in customers"
-                        :value="customer.id"
-                      >
-                        {{ customer.name }}
-                      </option>
-                    </select>
-                  </div>
-                  <div v-if="selectedCustomer" class="form-group">
-                    <label>Blok / Kavling </label>
-                    <select class="form-control"  @change="getHouse" v-model="selectedKavling">
-                      <option v-for="kavling in selectedCustomer.customerKavling"  :value="kavling.kavling.houseTypeId" :key="kavling">{{ kavling.kavling.block }} - {{ kavling.kavling.numberKavling }}</option>
-                    </select>
-                  </div>
-                  <div class="row mb-3" v-if="house">
-                    <div clas   s="col">
-                        <span>
-                            Tipe Rumah :
-                        </span>
-                        <Span>
-                            {{ house.houseType }}
-                        </Span>
+                    <div class="row">
+                        <div class="col" :class="{'col-12': !selectedCustomer}">
+
+                            <div class="form-group">
+                              <label>Nama</label>
+                              <select @change="selectCustomer(this)" ref="fieldCustomer" class="form-control" v-model="payment.customerId">
+                                <option
+                                  :key="customer.id"
+                                  v-for="customer in customers"
+                                  :value="customer.id"
+                                >
+                                  {{ customer.name }}
+                                </option>
+                              </select>
+                            </div>
+                        </div>
+                        <div class="col">
+                                 <div v-if="selectedCustomer" class="form-group">
+                        <label>Blok / Kavling </label>
+                        <select class="form-control"  @change="getHouse" v-model="selectedKavling">
+                          <option v-for="kavling in selectedCustomer.customerKavling"  :value="kavling.kavling" :key="kavling">{{ kavling.kavling.block }} - {{ kavling.kavling.numberKavling }}</option>
+                        </select>
+                      </div>
+                        </div>
                     </div>
+
+                  <div class="row " v-if="house">
                     <div class="col">
-                        <span>
-                            Harga :
-                        </span>
-                        <Span>
-                            {{ formatRupiah(house.price) }}
-                        </Span>
+                        <!-- <div class="form-group">
+                            <label>Referral</label>
+                            <select class="form-control" v-model="payment.type">
+                                <option value="Cash Keras">G0323J-Ahmad</option>
+                                <option value="Cash Keras">G0323J-Ahmad</option>
+                                <option value="Cash Keras">G0323J-Ahmad</option>
+                            </select>
+                          </div> -->
+                    </div>
+                    <div class="col m-auto">
+                        <div class="box d-flex">
+
+                            <div class="mr-5">
+                                <span>
+                                Tipe Rumah :
+                            </span>
+                            <Span>
+                                {{ house.houseType }}
+                            </Span>
+                            </div>
+                            <div>
+                             <span>
+                                Harga :
+                            </span>
+                            <Span>
+                                {{ formatRupiah(house.price) }}
+                            </Span>
+                            </div>
+                        </div>
                     </div>
                   </div>
-                  <div class="form-group">
-                    <label>Jenis Pembayaram</label>
-                    <select class="form-control" v-model="payment.type">
-                      <option value="Cash Keras">Cash Keras</option>
-                      <option value="Cash Bertahap">Cash Bertahap</option>
-                      <option value="KPR">KPR</option>
-                    </select>
-                  </div>
-                  <!-- <div class="form-group">
-                                <label for="name">Blok</label>
-                                <select  class="form-control"></select>
-                            </div> -->
-                  <div class="form-group">
-                    <label>Cicilan</label>
-                    <input
-                    v-model="payment.price"
-                    type="text"
-                    class="form-control"
-                    @change="formatRupiah(payment.price)"
-                    />
+                  <div class="row">
+                      <div class="col-6">
+                          <div class="form-group">
+                            <label>Jenis Pembayaram</label>
+                            <select class="form-control" v-model="payment.type">
+                              <option value="Cash Keras">Cash Keras</option>
+                              <option value="Cash Bertahap">Cash Bertahap</option>
+                              <option value="KPR">KPR</option>
+                            </select>
+                          </div>
+                      </div>
+                      <div class="col-6">
+                          <div class="form-group">
+                            <label>Setoran</label>
+                            <input
+                            v-model="payment.price"
+                            type="text"
+                            class="form-control"
+                            @change="formatRupiah(payment.price)"
+                            />
+                          </div>
+                      </div>
                   </div>
                 </div>
               </div>
@@ -106,6 +131,7 @@
                   data-toggle="collapse"
                   data-target="#formCreate"
                   class="btn btn-danger btn-block"
+                  @click="reset"
                 >
                   Batal
                 </button>
@@ -114,40 +140,6 @@
             </div>
 
           </div>
-          <!-- <div class="collapse" id="formEdit">
-            <div class="card">
-              <div class="card-header">
-                <h4>Form Angsuran {{ showNameCustomer(payment.customerId)}}</h4>
-              </div>
-              <div class="card-body">
-                <div class="form-col">
-                  <div class="form-group">
-                    <label>Cicilan</label>
-                    <input
-                      v-model="payment.price"
-                      type="text"
-                      class="form-control"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div class="card-body">
-                <button
-                  class="btn btn-primary btn-block"
-                  @click="editPayment"
-                >
-                  Simpan
-                </button>
-                <button
-                  data-toggle="collapse"
-                  data-target="#formEdit"
-                  class="btn btn-danger btn-block"
-                >
-                  Batal
-                </button>
-              </div>
-            </div>
-          </div> -->
           <div class="card">
             <div class="card-header">
               <Button
@@ -159,24 +151,9 @@
               </Button>
             </div>
             <div class="card-body">
-              <div class="row">
-                <div class="col-lg-8">
-                  <div class="btn-group-sm mb-3">
-                    <button
-                      data-toggle="modal"
-                      data-target="#modal-create"
-                      type="button"
-                      class="btn btn-icon icon-rigth btn-primary mr-2"
-                    >
-                      EXPORT PDF <i class="fa-solid fa-file-pdf"></i>
-                    </button>
-                    <button
-                      type="button"
-                      class="btn btn-icon icon-rigth btn-primary mr-2"
-                    >
-                      EXPORT EXCEL <i class="fa-solid fa-file-excel"></i>
-                    </button>
-                  </div>
+              <div class="row mb-2 d-flex justify-content-between" >
+                <div class="col-lg-6">
+                 <ButtonsExport :printData="false" />
                 </div>
                 <div class="col-lg-4">
                   <input
@@ -187,16 +164,17 @@
                 </div>
               </div>
               <div class="table-responsive">
-                <table class="table table-striped">
+                <table class="table table-striped" style="font-size:12px">
                   <thead>
                     <tr>
-                      <th width="20%">Nama</th>
-                      <th width="5%">Blok</th>
-                      <th width="10%">Nomor telepon</th>
-                      <th width="10%">Angsuran</th>
-                      <th width="15%">Sisa Angsuran</th>
-                      <th width="10%">Tipe Pembayaran</th>
-                      <th class="text-center" width="5%">Aksi</th>
+                      <th >Nama</th>
+                      <th >Nomor telepon</th>
+                      <th >Blok / Kavling</th>
+                      <th >Tipe Rumah</th>
+                      <th >Harga Rumah</th>
+                      <th >Sisa Angsuran</th>
+                      <th >Tipe Pembayaran</th>
+                      <th class="text-center" >Aksi</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -205,7 +183,7 @@
                         <div class="media">
                           <figure class="avatar mr-2 avatar-md">
                             <img
-                              src="../../../../public/assets/images/avatar/avatar-1.png"
+                              :src="'storage/' + payment.customer.image"
                               alt="..."
                             />
                             <i
@@ -227,10 +205,11 @@
                           </div>
                         </div>
                       </td>
-                      <td>{{ getBlocks(payment.customer.customerKavling) }}</td>
                       <td>{{ payment.customer.phone }}</td>
-                      <td>{{ formatRupiah(payment.priceHouse) }}</td>
-                      <td>{{ formatRupiah(payment.reminderPayment) }}</td>
+                      <td>{{ payment.block.block}}-{{ payment.block.numberKavling }}</td>
+                      <td>{{ payment.houseType.houseType}}</td>
+                      <td>{{ formatRupiah(payment.houseType.price) }}</td>
+                      <td>{{  formatRupiah(payment.reminderPayment)}}</td>
                       <td>
                         <span
                           :class="
@@ -255,14 +234,14 @@
                             ></i>
                           </button>
                           <div class="dropdown-menu action">
-                            <!-- <button
+                            <button
                               class="dropdown-item action sortable"
                               data-toggle="collapse"
-                              data-target="#formEdit"
+                              data-target="#formCreate"
                               @click="sendEdit(payment)"
                             >
-                              Nyicil
-                            </button> -->
+                              Setor
+                            </button>
                             <button class="dropdown-item action sortable">
                               Detail
                             </button>
@@ -296,11 +275,13 @@ import iziToast from "izitoast";
 import Pagination from "../../components/Pagination.vue";
 import CircleLoader from "../../components/CircleLoader.vue";
 import Utils from "../../store/services/utils";
+import ButtonsExport from "../../components/ButtonsExport.vue";
 export default {
   components: {
     Pagination,
     CircleLoader,
-  },
+    ButtonsExport
+},
   data() {
     return {
       //data
@@ -308,6 +289,7 @@ export default {
         id: "",
         customerId: "",
         employeeId: "",
+
         type: "",
         price: "",
       },
@@ -320,7 +302,6 @@ export default {
       selectedCustomer: null,
       selectedKavling: null,
       isLoading: false,
-      selectedCustomer: null,
       typePayment: [
         {
           type: "Cash Keras",
@@ -345,6 +326,12 @@ export default {
       },
     };
   },
+  watch: {
+    selectedCustomer(newVal){
+        this.selectedCustomer = newVal
+
+    }
+  },
   mounted() {
     this.getPayments();
     this.getCustomers();
@@ -362,11 +349,10 @@ export default {
 
     getHouse(){
     const self = this;
-      this.$store.dispatch("getData", ["house-type/"+ this.selectedKavling]).then((res) => {
+      this.$store.dispatch("getData", ["house-type/"+ this.selectedKavling.houseTypeId]).then((res) => {
             this.house = res.data
       });
     },
-
     selectCustomer(){
         this.customers.filter((cs)=>{
             if(cs.id == this.$refs['fieldCustomer'].value){
@@ -374,8 +360,6 @@ export default {
             }
         })
     },
-
-
     showNameCustomer(id){
             let name =''
             this.customers.forEach((customer) => {
@@ -397,6 +381,9 @@ export default {
     formatRupiah(number) {
       return Utils.formatRupiah(number, "Rp. ");
     },
+    calcReferral(percent , number){
+        return Utils.calcReferral(percent , number)
+    },
     showLogUpdate(date) {
       return moment(date).fromNow();
     },
@@ -409,11 +396,12 @@ export default {
       const params = [
         //   `name=${this.name}`,
         // `position=${this.name}`,
-        // `order_direction=${this.order_direction}`,
-        // `page=${this.pagination.page}`,
-        // `per_page=${this.pagination.perPage}`,
+        `order_direction=${this.order_direction}`,
+        `page=${this.pagination.page}`,
+        `per_page=${this.pagination.perPage}`,
       ].join("&");
       self.$store.dispatch("getData", ["payment", params]).then((res) => {
+        console.log(res.data)
         self.payments = res.data;
         self.pagination.total = res.meta.total;
         self.pagination.currentPage = res.meta.currentPage;
@@ -442,7 +430,7 @@ export default {
         {
           customer_id: this.payment.customerId,
           employee_id: this.idEmployee,
-          houseTypeId: this.selectedKavling,
+          houseTypeId: this.selectedKavling.houseTypeId,
           type: this.payment.type,
           price: this.payment.price,
         },
@@ -493,8 +481,9 @@ export default {
         )
     },
     sendEdit(payment){
-        this.payment.id = payment.id
-        this.payment.customerId= payment.customer.id
+        // this.payment.customerId= payment.customer.id
+
+        this.selectedCustomer = payment.customer
     },
     delete(id) {
       const self = this;
@@ -529,13 +518,30 @@ export default {
             employeeId: "",
             type: "",
             price: "",
-        }
+        },
+        this.selectedCustomer = null
+        this.selectedKavling = null
+        this.house = null
+
     }
   },
 };
 </script>
-<style>
+<style >
+    table th {
+
+    padding: 0 15px !important;
+    height: 60px;
+    vertical-align: middle;
+}
 .avatar .avatar-presence {
   top: 0 !important;
+}
+.box {
+  border: 1px solid #e5e5e5;
+  border-radius: 5px;
+  padding: 10px;
+  background-color: #E6EFFF;
+  margin: auto;
 }
 </style>
