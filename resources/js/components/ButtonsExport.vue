05d89d8608f047f7d110e72365e942e4c08ee049
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-07 16:00:28 +0700

    🔨🔨🔨Create Components

---
+++ resources/js/components/ButtonsExport.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="" :class="'btn-group-'+size">
+    <button
+
+      type="button"
+      class="btn btn-icon icon-rigth btn-primary mr-2"
+        @click="exportPdf"
+    >
+      EXPORT PDF <i class="fa-solid fa-file-pdf"></i>
+    </button>
+    <button type="button" class="btn btn-icon icon-rigth btn-primary mr-2" @click="exportExcel">
+      EXPORT EXCEL <i class="fa-solid fa-file-excel"></i>
+    </button>
+    <button v-if="printData" @click="print" type="button" class="btn btn-icon icon-rigth btn-primary mr-2">
+      PRINT DATA <i class="fa-solid fa-print"></i>
+    </button>
+  </div>
+</template>
+<script>
+export default {
+    props:{
+        printData : {
+            type : Boolean,
+            default : true
+        },
+        size: {
+            type: String,
+            default: 'sm'
+        }
+    },
+    methods:{
+        print(){
+            this.$emit('print')
+        },
+        exportPdf(){
+            this.$emit('exportPdf')
+        },
+        exportExcel(){
+            this.$emit('exportExcel')
+        }
+    }
+};
+</script>
+<style lang="">
+</style>
