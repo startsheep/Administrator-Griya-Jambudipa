cc67ae7effcc2ef88f558720dad2305dd1dbb706
hakim-asrori <saneglos005@gmail.com>
2022-09-08 15:40:30 +0700

    export customer

---
+++ app/Http/Controllers/Export/CustomerController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Export;
+
+use App\Exports\CustomerExport;
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+
+class CustomerController extends Controller
+{
+    public function pdf()
+    {
+        return Excel::download(new CustomerExport, date("Y_m_d_His") . '_REKAP_DATA_CUSTOMER.pdf');
+    }
+
+    public function excel()
+    {
+        return Excel::download(new CustomerExport, date("Y_m_d_His") . '_REKAP_DATA_CUSTOMER.xlsx');
+    }
+
+    public function print()
+    {
+        $data = [
+            'customers' => Customer::all()
+        ];
+
+        return view('exports.customers.excel', $data);
+    }
+}
--- resources/views/exports/employees/excel.blade.php
+++ resources/views/exports/employees/excel.blade.php
@@ -24,7 +24,8 @@
             <th>Divisi</th>
             <th>Gaji</th>
             <th>Status Keaktifan</th>
-            <th>Status Kepagawaian</th>
+            <th>Status Kepegawaian</th>
+            <th>Foto</th>
         </tr>
     </thead>
     <tbody>
@@ -33,9 +34,21 @@
                 <th>{{ $loop->iteration }}</th>
                 <td>{{ $employee->name }}</td>
                 <td>{{ $employee->position->name }}</td>
-                <td>Rp. {{ number_format($employee->position->salary, 0, ',', '.') }}</td>
+                @if ($employee->position->name == 'Marketing')
+                    @php
+                        $total = 0;
+                        foreach ($employee->payment as $salary) {
+                            $total += $salary->commission;
+                        }
+                    @endphp
+                    <td>Rp. {{ number_format($total, 0, ',', '.') }}</td>
+                @else
+                    <td>Rp. {{ number_format($employee->position->salary, 0, ',', '.') }}</td>
+                @endif
                 <td>{{ $employee->is_active == 1 ? 'Aktif' : 'Non-Aktif' }}</td>
                 <td>{{ $employee->status == 1 ? 'Pegawai Tetap' : 'Pegawai Tidak Tetap' }}</td>
+                <td><img src="{{ url('storage/' . $employee->image) }}" alt="{{ $employee->name }}" height="100"
+                        width="100"></td>
             </tr>
         @endforeach
     </tbody>
