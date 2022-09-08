34a9f60b8da8756c4e9b645e3758d601413db380
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-08 22:26:20 +0700

    ⬆️⬆️reusable component

---
+++ resources/js/components/Media.vue
@@ -0,0 +1,39 @@
+<template>
+  <li class="media p-0 ">
+    <div class="media-left">
+        <slot name="left"></slot>
+    </div>
+    <div class="media-body">
+      <div class="media-right m-auto">
+        <slot name="right"></slot>
+      </div>
+      <div class="media-title">
+        <slot name="mediaTittle"></slot>
+      </div>
+      <div class="text-small text-muted">
+        {{ subtitle }}
+      </div>
+    </div>
+  </li>
+</template>
+<script>
+export default {
+    props: {
+        mediaRight: {
+            type: String,
+            default: ''
+        },
+        mediaTittle: {
+            type: String,
+            default: ''
+        },
+        subtitle: {
+            type: String,
+            default: ''
+        }
+    },
+
+};
+</script>
+<style>
+</style>
