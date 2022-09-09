9dd50b1090cc683066f592e108e023ef5359eeb5
hakim-asrori <saneglos005@gmail.com>
2022-09-09 12:41:56 +0700

    menambahkan status dan tombol cetak di print preview

---
+++ database/migrations/2022_09_09_033931_add_column_in_contractors.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contractors', function (Blueprint $table) {
+            $table->integer('status')->default(1)->nullable()->after('image');
+        });
+    }
+};
--- resources/views/exports/customers/excel.blade.php
+++ resources/views/exports/customers/excel.blade.php
@@ -1,75 +1,41 @@
-<style>
-    table {
-        border-collapse: collapse;
-        width: 100%;
-    }
+@extends('exports.template')
 
-    table,
-    th,
-    td {
-        border: 1px solid black;
-    }
-
-    th,
-    td {
-        padding: 10px;
-    }
-</style>
-
-<table>
-    <thead>
-        <tr>
-            <th>No.</th>
-            <th>Nama</th>
-            <th>Email</th>
-            <th>No. Telepon</th>
-            <th>Profesi</th>
-            <th>Jenis Kelamin</th>
-            <th>Alamat</th>
-            <th>Status</th>
-            <th>Blok/Kavling</th>
-            <th>Foto</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($customers as $customer)
+@section('content')
+    <table>
+        <thead>
             <tr>
-                <th>{{ $loop->iteration }}</th>
-                <td>{{ $customer->name }}</td>
-                <td><a href="mailto:{{ $customer->email }}">{{ $customer->email }}</a></td>
-                <td><a href="tel:{{ $customer->phone }}">{{ $customer->phone }}</a></td>
-                <td>{{ $customer->profession }}</td>
-                <td>{{ $customer->gender == 'L' ? 'Laki-laki' : 'Perempuan' }}</td>
-                <td>{{ $customer->address }}</td>
-                <td>{{ $customer->is_active == 1 ? 'Aktif' : 'Non-Aktif' }}</td>
-                <td>
-                    @foreach ($customer->customerKavling as $kavling)
-                        {{ $kavling->kavling->block . '-' . $kavling->kavling->number_kavling . '/' . $kavling->kavling->houseType->house_type }}
-                    @endforeach
-                </td>
-                <td><img src="{{ url('storage/' . $customer->image) }}" alt="{{ $customer->name }}" height="100"
-                        width="100"></td>
+                <th>No.</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>No. Telepon</th>
+                <th>Profesi</th>
+                <th>Jenis Kelamin</th>
+                <th>Alamat</th>
+                <th>Status</th>
+                <th>Blok/Kavling</th>
+                <th>Foto</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
-
-<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"
-    integrity="sha512-3j3VU6WC5rPQB4Ld1jnLV7Kd5xr+cq9avvhwqzbH/taCRNURoeEpoPBK9pDyeukwSxwRPJ8fDgvYXd6SkaZ2TA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-<script>
-    let token = $.cookie('token')
-
-    if (!token) {
-        $("table").html('');
-        location.href = "/auth/login";
-    }
-</script>
-
-@if (Request::segment(3) == 'print')
-    <script>
-        // window.print()
-    </script>
-@endif
+        </thead>
+        <tbody>
+            @foreach ($customers as $customer)
+                <tr>
+                    <th>{{ $loop->iteration }}</th>
+                    <td>{{ $customer->name }}</td>
+                    <td><a href="mailto:{{ $customer->email }}">{{ $customer->email }}</a></td>
+                    <td><a href="tel:{{ $customer->phone }}">{{ $customer->phone }}</a></td>
+                    <td>{{ $customer->profession }}</td>
+                    <td>{{ $customer->gender == 'L' ? 'Laki-laki' : 'Perempuan' }}</td>
+                    <td>{{ $customer->address }}</td>
+                    <td>{{ $customer->is_active == 1 ? 'Aktif' : 'Non-Aktif' }}</td>
+                    <td>
+                        @foreach ($customer->customerKavling as $kavling)
+                            {{ $kavling->kavling->block . '-' . $kavling->kavling->number_kavling . '/' . $kavling->kavling->houseType->house_type }}
+                        @endforeach
+                    </td>
+                    <td><img src="{{ url('storage/' . $customer->image) }}" alt="{{ $customer->name }}" height="100"
+                            width="100"></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
