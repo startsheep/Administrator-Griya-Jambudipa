ce2ab10633055f7275a0bd768a216291a0b237c6
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-09 22:42:17 +0700

    🔨🔨🔨

--- resources/js/pages/land/FormChild.vue
+++ resources/js/pages/land/FormChild.vue
@@ -35,23 +35,21 @@
           class="form-control form-control-md m-2"
         />
         <input
-          type="text"
+          type="number"
           disabled
           v-model="landPrice.total"
           placeholder="Total"
           class="form-control form-control-md m-2"
         />
         <button
-          @click="
-            isEdit ? update() : submit()
-          "
+          @click="isEdit ? update() : submit()"
           class="btn btn-primary btn-md mr-1"
         >
           <i class="fas fa-plus-square"></i>
         </button>
         <button
           data-toggle="collapse"
-          :data-target="'#formChild' +idForm    "
+          :data-target="'#formChild' + idForm"
           class="btn btn-danger btn-md"
           @click="resetForm()"
         >
@@ -63,54 +61,73 @@
 </template>
 <script>
 export default {
-    props:{
-        idForm : {
-           type: String ,
-           default: ''
-        },
-        isEdit:{
-            type:Boolean ,
-            default : false
-        },
+  props: {
+    idForm: {
+      type: String,
+      default: "",
+    },
+    isEdit: {
+      type: Boolean,
+      default: false,
+    },
+    basicPrice: {
+      type: Object,
+      default: {
+        description: "",
+        volume: "",
+        unit: "",
+        price: "",
+        amount: "",
+        total: "",
+      },
+    },
+  },
+  watch: {
+    basicPrice: {
+      handler: function (val) {
+        this.landPrice = val;
+      },
+      deep: true,
     },
 
-    data(){
-        return {
-             landPrice: {
-                categories: '',
-                id: '',
-                description: '',
-                volume: '',
-                unit: '',
-                price: '',
-                amount: '',
-                total: '',
-            },
-        }
-    },
 
-    methods:{
-        submit(){
-            this.$emit('onSubmit' , this.landPrice)
-        },
-        sumTotal() {
-      this.landPrice.total =
-        this.landPrice.amount * this.landPrice.price;
+  },
+  data() {
+    return {
+      landPrice: {
+        categories: "",
+        id: "",
+        description: "",
+        volume: "",
+        unit: "",
+        price: "",
+        amount: "",
+        total: "",
+      },
+    };
+  },
+
+  methods: {
+    submit() {
+      this.$emit("onSubmit", this.landPrice);
     },
-        resetForm(){
-            this.landPrice = {
-                categories: '',
-                id: '',
-                description: '',
-                volume: '',
-                unit: '',
-                price: '',
-                amount: '',
-                total: '',
-            },
-            this.$emit('onResetForm')
-        },
-    }
+    sumTotal() {
+      this.landPrice.total = this.landPrice.amount * this.landPrice.price;
+    },
+    resetForm() {
+      (this.landPrice = {
+        categories: "",
+        id: "",
+        description: "",
+        volume: "",
+        unit: "",
+        price: "",
+        amount: "",
+        total: "",
+      }),
+        this.$emit("onResetForm");
+    },
+  },
 };
 </script>
 <style >
