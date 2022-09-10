14ae8f5b4e3440f67876d36ca6243edadd7f5bf6
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-10 09:32:17 +0700

    ✅✅

--- resources/js/pages/dashboard/RecentActivity.vue
+++ resources/js/pages/dashboard/RecentActivity.vue
@@ -5,70 +5,22 @@
     </div>
     <div class="card-body">
       <ul class="list-unstyled list-unstyled-border">
-        <li class="media">
+</ul>        <li class="media" v-for="activity in recentActivity" :key="activity">
           <img
             class="mr-3 rounded-circle"
             width="50"
-            src="assets/img/avatar/avatar-1.png"
+            :src="activity.avatar"
             alt="avatar"
           />
           <div class="media-body">
-            <div class="float-right text-primary">Now</div>
-            <div class="media-title">Farhan A Mujib</div>
+            <div class="float-right text-primary">{{ activity.time }}</div>
+            <div class="media-title">{{ activity.name }}</div>
             <span class="text-small text-muted"
-              >Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-              scelerisque ante sollicitudin.</span
+              >{{ activity.activity     }}</span>
             >
           </div>
         </li>
-        <li class="media">
-          <img
-            class="mr-3 rounded-circle"
-            width="50"
-            src="assets/img/avatar/avatar-2.png"
-            alt="avatar"
-          />
-          <div class="media-body">
-            <div class="float-right">12m</div>
-            <div class="media-title">Ujang Maman</div>
-            <span class="text-small text-muted"
-              >Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-              scelerisque ante sollicitudin.</span
-            >
-          </div>
-        </li>
-        <li class="media">
-          <img
-            class="mr-3 rounded-circle"
-            width="50"
-            src="assets/img/avatar/avatar-3.png"
-            alt="avatar"
-          />
-          <div class="media-body">
-            <div class="float-right">17m</div>
-            <div class="media-title">Rizal Fakhri</div>
-            <span class="text-small text-muted"
-              >Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-              scelerisque ante sollicitudin.</span
-            >
-          </div>
-        </li>
-        <li class="media">
-          <img
-            class="mr-3 rounded-circle"
-            width="50"
-            src="assets/img/avatar/avatar-4.png"
-            alt="avatar"
-          />
-          <div class="media-body">
-            <div class="float-right">21m</div>
-            <div class="media-title">Alfa Zulkarnain</div>
-            <span class="text-small text-muted"
-              >Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-              scelerisque ante sollicitudin.</span
-            >
-          </div>
-        </li>
+
       </ul>
       <div class="text-center pt-1 pb-1">
         <a href="#" class="btn btn-primary btn-lg btn-round"> View All </a>
@@ -77,7 +29,70 @@
   </div>
 </template>
 <script>
-export default {};
+export default {
+    data(){
+        return {
+            recentActivity: [
+                {
+                    id: 1,
+                    avatar:'https://robohash.org/stefan-oneapi.lorem.space/image/face?w=150&h=150',
+                    name: 'Farhan A Mujib',
+                    activity: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.',
+                    time: 'Now'
+                },
+                {
+                    id: 2,
+                    avatar:'https://robohash.org/stefan-oneapi.lorem.space/image/face?w=150&h=150',
+                    name: 'Ujang Maman',
+                    activity: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.',
+                    time: '2 Hours Ago'
+                },
+                {
+                    id: 3,
+                    avatar:'https://robohash.org/stefan-oneapi.lorem.space/image/face?w=150&h=150',
+                    name: 'Rizal Fakhri',
+                    activity: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.',
+                    time: '5 Hours Ago'
+                },
+                {
+                    id: 4,
+                    avatar:'https://robohash.org/stefan-oneapi.lorem.space/image/face?w=150&h=150',
+                    name: 'Alfa Zulkarnain',
+                    activity: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.',
+                    time: '12 Hours Ago'
+                },
+                {
+                    id: 5,
+                    avatar:'https://robohash.org/stefan-oneapi.lorem.space/image/face?w=150&h=150',
+                    name: 'Shayna Anne',
+                    activity: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.',
+                    time: '15 Hours Ago'
+                },
+                {
+                    id: 6,
+                    avatar:'https://robohash.org/stefan-oneapi.lorem.space/image/face?w=150&h=150',
+                    name: 'Farhan A Mujib',
+                    activity: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.',
+                    time: 'Now'
+                },
+                {
+                    id: 7,
+                    avatar:'https://robohash.org/stefan-oneapi.lorem.space/image/face?w=150&h=150',
+                    name: 'Ujang Maman',
+                    activity: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.',
+                    time: '2 Hours Ago'
+                },
+                {
+                    id: 8,
+                    avatar:'https://robohash.org/stefan-oneapi.lorem.space/image/face?w=150&h=150',
+                    name: 'Rizal Fakhri',
+                    activity: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.',
+                    time: '5 Hours Ago'
+                },
+            ]
+        }
+    },
+};
 </script>
 <style>
 </style>
