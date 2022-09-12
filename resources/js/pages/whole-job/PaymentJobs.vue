036313ec68a16a102387d12c225073b8f65085ec
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-12 19:17:19 +0700

    ⬆️⬆️⬆️Create basic payment jobs

---
+++ resources/js/pages/whole-job/PaymentJobs.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="section">
+    <div class="section-header"><h1>Pembayaran Borongan</h1></div>
+    <div class="row m-auto">
+        <div class="col">
+            <h4>Coming Soon</h4>
+        </div>
+    </div>
+   </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
