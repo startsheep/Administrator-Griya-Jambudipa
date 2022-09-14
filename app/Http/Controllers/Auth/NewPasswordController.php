0b8fad6d5cd5d26b3e30dee872cebd52d6e06dbd
hakim-asrori <saneglos005@gmail.com>
2022-09-14 20:51:18 +0700

    email service

---
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+
+class NewPasswordController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        if ($status === Password::PASSWORD_RESET) {
+            return response()->json([
+                'message' => 'Kata sandi berhasil diubah'
+            ]);
+        } else {
+            return response()->json([
+                'meta' => [
+                    'message' =>  __($status),
+                    'status_code' => 400,
+                ]
+            ], 400);
+        }
+
+        return response()->json(['test' => 'ok']);
+    }
+}
