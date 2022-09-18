5f5e0701274067bb17fdc5f163152933cf00e08f
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-18 21:38:24 +0700

    ⬆️⬆️⬆️Base

---
+++ resources/js/pages/whole-job/JobFacility.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="section">
+        <div class="section-header">
+            <h1>Borongan Fasilitas Umum</h1>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
