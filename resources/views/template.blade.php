32410962c1e714b98d5a56f6adac1f9748f9e464
hakim-asrori <saneglos005@gmail.com>
2022-09-19 21:48:35 +0700

    Memunculkan Image

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('assets/fontawesome-free-6.1.2-web/css/all.min.css') }}">
-    <link rel="shortcut icon" href="{{ asset('assets/images/logo.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo.png') }}" id="logo" />
     <title>Administrator Griya Jambudipa</title>
 </head>
 
@@ -20,22 +20,6 @@
     <script src="{{ asset('assets/js/stisla.js') }}"></script>
     <script src="{{ asset('assets/js/scripts.js') }}"></script>
     <script src="{{ asset('assets/js/custom.js') }}"></script>
-    <script>
-        loadProfile();
-
-        function loadProfile() {
-            $.get("/api/company-profile", function(response) {
-                $(document).attr("title", response.data.name)
-                if (response.data.logo == null) {
-                    $("link[rel='shortcut icon']").attr("href", "{{ asset('assets/images/logo.png') }}")
-                    $("#logo").attr("src", "{{ asset('assets/images/logo.png') }}")
-                } else {
-                    $("link[rel='shortcut icon']").attr("href", '/storage/' + response.data.logo)
-                    $("#logo").attr("src", '/storage/' + response.data.logo)
-                }
-            })
-        }
-    </script>
 </body>
 
 </html>
