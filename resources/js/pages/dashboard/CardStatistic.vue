41b3e17238db5933cd8b3f6ff95e6788cffb7308
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-24 10:05:14 +0700

    ⬆️⬆️⬆️Update component

--- resources/js/pages/dashboard/CardStatistic.vue
+++ resources/js/pages/dashboard/CardStatistic.vue
@@ -1,38 +1,39 @@
 <template>
-
-    <div class="card card-statistic-1">
-      <div class="card-icon " :class="'bg-'+backgroundColor">
-        <i :class="icon"></i>
+  <div class="card card-statistic-1">
+    <div class="card-icon" :class="'bg-' + backgroundColor">
+      <i :class="icon"></i>
+    </div>
+    <div class="card-wrap">
+      <div class="card-header">
+        <h4>{{ tittle }}</h4>
       </div>
-      <div class="card-wrap">
-        <div class="card-header">
-          <h4>{{ tittle }}</h4>
-        </div>
-        <div class="card-body">{{ count }}</div>
+      <div class="card-body">
+        <span class="count-value">{{ count }}</span>
       </div>
     </div>
-
+  </div>
 </template>
 <script>
+
 export default {
-    props:{
-        tittle: {
-            type: String,
-            default: "Tittle",
-        },
-        icon: {
-            type: String,
-            default: "fas fa-user",
-        },
-        count: {
-            type: Number,
-            default: 0,
-        },
-        backgroundColor: {
-            type: String,
-            default: "primary",
-        },
-    }
+  props: {
+    tittle: {
+      type: String,
+      default: "Tittle",
+    },
+    icon: {
+      type: String,
+      default: "fas fa-user",
+    },
+    count: {
+      type: Number,
+      default: 0,
+    },
+    backgroundColor: {
+      type: String,
+      default: "primary",
+    },
+  },
 };
 </script>
 <style>
