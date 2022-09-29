e0fdc427a32c011620d87926d55a81ade82a039e
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-09-29 09:39:37 +0700

    ⬆️⬆️⬆️Add Route name

--- resources/js/router/services/auth.js
+++ resources/js/router/services/auth.js
@@ -13,7 +13,9 @@
 
 export default [{
         path: '/auth/login',
+        name: 'Login',
         component: () => import('../../pages/auth/Login.vue'),
+
         meta: {
             middleware: 'guest',
         }
