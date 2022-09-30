fb4af39cb36705fa818f3965c31e2d8386b0f941
monarosa22 <monarosanah66@gmail.com>
2022-09-30 22:24:33 +0700

    feature/AGJ-209 ubah profile picture pada log dashboard

--- resources/js/pages/dashboard/RecentActivity.vue
+++ resources/js/pages/dashboard/RecentActivity.vue
@@ -13,10 +13,10 @@
                         :key="activity"
                     >
                         <img
-                            class="mr-3 rounded-circle"
-                            width="50"
-                            src="https://robohash.org/stefan-oneapi.lorem.space/image/face?w=150&h=150"
-                            alt="avatar"
+                            class="rounded-circle mr-3 mt-1"
+                            style="width:50px; height:50px"
+                            :src="'storage/' + activity.user.image.documentPath"
+                            alt=""
                         />
                         <div class="media-body">
                             <div class="float-right text-primary">
