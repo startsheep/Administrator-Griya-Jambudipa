e80eebddf4b603bdddc7dbb3a1c24bb32cbceab1
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-10-02 22:31:42 +0700

    🔨🔨🔨Fix

--- resources/js/pages/dashboard/KavlingHasPerson.vue
+++ resources/js/pages/dashboard/KavlingHasPerson.vue
@@ -14,11 +14,10 @@
       </div>
       <div class="col-lg-2">
         <div class="card-rounded">
-          <select class="form-control" v-model="block" >
-            <option value="">Pilih Blok</option>
-            <!-- <option v-for="item in blocks" :value="item">
-              {{ item }}
-            </option> -->
+          <select class="form-control" @change="filterByStatus" v-model="status">
+            <option value="">Pilih Status</option>
+            <option value="1">Terjual</option>
+            <option value="0">Belum Terjual</option>
           </select>
         </div>
       </div>
@@ -45,10 +44,10 @@
           <!-- select block -->
         </div>
       </div>
-
     </div>
-    <div class="row mt-3">
+    <div class="row mt-3" v-auto-animate>
       <div
+
         class="col-lg-3 col-md-4 col-sm-6 col-12"
         v-for="kavling in kavlings"
         :key="kavling.id"
@@ -56,14 +55,22 @@
         <div class="card">
           <div class="card-header pb-0">
             <h4 class="card-title d-block">Kavling blok {{ kavling.block }}</h4>
+            <!-- actions header -->
+                <div class="actions-header">
+                    l
+                </div>
           </div>
           <div class="card-body pt-0">
             <div class="row">
               <div class="col-12">
                 <strong>Tipe Rumah : </strong>
-                <span>dsfd</span>
+                <span>{{ kavling.houseType.houseType }}</span>
               </div>
 
+              <div class="col-12">
+                <strong>Nomor Kavling : </strong>
+                <span>{{ kavling.numberKavling }}</span>
+              </div>
               <div class="col-12">
                 <strong>Panjang Kavling : </strong>
                 <span>{{ kavling.lengthKavling }}</span>
@@ -81,6 +88,13 @@
                 <span>{{ kavling.widthKavling }}</span>
               </div>
             </div>
+          </div>
+          <div class="card-footer">
+            <span
+              class="badge d-block"
+              :class="kavling.status == 1 ? 'badge-success' : 'badge-info'"
+              >{{ kavling.status == 1 ? "Terjual" : "Belum Terjual" }}</span
+            >
           </div>
         </div>
       </div>
@@ -103,43 +117,65 @@
 </style>
 <script >
 import LoadingComponent from "../../components/LoadingComponent.vue";
-export default{
-    data(){
-        return{
-            kavlings:[],
-            search:'',
-            block:'',
-            blocks:[
-                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
-            ]
-,
-            loading:false,
-        }
+export default {
+  data() {
+    return {
+      kavlings: [],
+      search: "",
+      block: "",
+      status: "",
+      blocks: [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+      ],
+      loading: false,
+    };
+  },
+  components: {
+    LoadingComponent,
+  },
+  methods: {
+    getData() {
+      this.loading = true;
+      this.$store.dispatch("getData", ["dashboard/kavling"]).then((res) => {
+        this.kavlings = res.data.kavlings;
+        this.loading = false;
+      });
     },
-    components:{
-        LoadingComponent
+    filterByStatus(){
+        // this.kavlings = this.kavlings.filter(status => status.status == this.status);
     },
-    methods:{
-        getData(){
-            this.loading=true
-            this.$store.dispatch('getData', ['dashboard/kavling']).then(res=>{
-                this.kavlings = res.data.kavlings
-                this.loading=false
-            })
-        },
-        // make infinite scrollTop and load data
-        filterByBlock(){
+    filterByBlock(){
 
-            this.getData();
-            setTimeout(()=> {
-                this.kavlings = this.kavlings.filter(kavling=>kavling.block == this.block)
-            }, 1000)
-            this.loading=false
-        },
-    },
-    mounted(){
-        this.getData()
     }
-
-}
+    // make infinite scrollTop and load data
+  },
+  mounted() {
+    this.getData();
+  },
+};
 </script>
