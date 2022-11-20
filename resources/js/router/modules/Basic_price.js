256fe5e35f386f63306045ad60b3a559a2fb597b
hakim-asrori <saneglos005@gmail.com>
2022-11-20 21:08:46 +0700

    rename file

--- resources/js/router/modules/Basic_price.js
+++ resources/js/router/modules/Basic_price.js
@@ -1,12 +1,12 @@
 export default [
     {
-        path:'/Basic-price-building',
-        name:'Building',
-        component: () => import('../../pages/land/BasicPrice.vue')
+        path: "/Basic-price-building",
+        name: "Building",
+        component: () => import("../../pages/land/BasicPrice.vue"),
     },
     {
-        path:'/Basic-price-land',
-        name:'Land',
-        component: () => import('../../pages/land/BasicPrice.vue')
-    }
-]
+        path: "/Basic-price-land",
+        name: "Land",
+        component: () => import("../../pages/land/BasicPrice.vue"),
+    },
+];
