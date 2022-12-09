90dce2b6b936d8a3c1139cdadb188d67e21ed210
hakim-asrori <saneglos005@gmail.com>
2022-12-09 20:54:14 +0700

    Refactor login

---
+++ app/Http/Repositories/User/UserRepositoryImplement.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Repositories\User;
+
+use App\Http\Services\Traits\QueryWhere;
+use LaravelEasyRepository\Implementations\Eloquent;
+use App\Models\User;
+
+class UserRepositoryImplement extends Eloquent implements UserRepository
+{
+    /**
+     * Model class to be used in this repository for the common methods inside Eloquent
+     * Don't remove or change $this->model variable name
+     * @property Model|mixed $model;
+     */
+    protected $model;
+
+    public function __construct(User $model)
+    {
+        $this->model = $model;
+    }
+
+    public function findByCriteria(array $criteria): User
+    {
+        return $this->model->where($criteria)->first();
+    }
+}
---
+++ config/easy-repository.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    /**
+     * The directory for all the repositories
+     */
+    "repository_directory" => "app/Http/Repositories",
+
+    /**
+     * Default repository namespace
+     */
+    "repository_namespace" => "App\Http\Repositories",
+
+    /**
+     * The directory for all the services
+     */
+    "service_directory" => "app/Services",
+
+    /**
+     * Default service namespace
+     */
+    "service_namespace" => "App\Services",
+];
