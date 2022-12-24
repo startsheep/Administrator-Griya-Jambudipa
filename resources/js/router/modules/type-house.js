f88c649b7809c77b840b342c821a6bbf01028bd2
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-12-24 12:31:30 +0700

    ⬆️⬆️⬆️latest update

--- resources/js/router/modules/type-house.js
+++ resources/js/router/modules/type-house.js
@@ -1,6 +1,13 @@
-export default [
-    {
+export default [{
         path: "/type-house",
         component: () => import("../../pages/type-house/House.vue"),
+    },
+    {
+        name: "detail-type-house",
+
+        path: "/type-house/:id/detail",
+        component: () => import("../../pages/type-house/DetailTypeHouse.vue"),
+        props: true,
     }
+
 ]
